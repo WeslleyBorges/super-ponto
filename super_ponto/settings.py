e69f0640800c222257c1e4b69c31308c5225e8fc
Weslley <weslleynfs@gmail.com>
2019-05-28 00:09:25 -0400

    Substituição de url ao ALLOWED_HOSTS

--- super_ponto/settings.py
+++ super_ponto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['superponto.superensino.com.br']
+ALLOWED_HOSTS = ['super-ponto.herokuapp.com']
 
 
 # Application definition
